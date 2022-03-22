ba7ef8ac57773766ea7f3766b78844787ee59ae9
farqlia <68340482+farqlia@users.noreply.github.com>
2022-03-22 10:22:17 +0100

    cleaning part 1

--- src/main/java/lab1/runner/Main.java
+++ src/main/java/lab1/runner/Main.java
@@ -1,85 +1,51 @@
 package lab1.runner;
 
-import lab1.processesgenerator.Generator;
-
 import java.util.Locale;
 import java.util.Scanner;
 
-
 public class Main {
-/*
+
+    private static final int quantaTime = 1;
+    private static final int minDuration = 50;
+    private static final int difference = 50;
+    private static final int intensity = 2;
+    private static final int iterations = 100000;
+    private static final boolean showStatistics = false;
+
+    // For simulation 2
+    private static final double phase = 0.1;
+    private static final double phaseLength = 0.1;
+    private static final int multiplicant = 10;
+
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         boolean isDone = false;
-        Generator generator;
 
-        AbstractSimulation simulation = null;
+        AbstractSimulation simulation;
 
-        while (!isDone){
+        while (!isDone) {
             System.out.println("Choose algorithm: 1.FCFS\n2.SJF\n3.RR\n4.All\n5.exit(e)");
             String input = scanner.nextLine();
 
-            if ('e' == input.charAt(0)){
+            if ('e' == input.charAt(0)) {
                 isDone = true;
             } else {
-                System.out.println("Show statistics: (t/f)");
-                boolean showStatistics = "t".equals(scanner.nextLine().toLowerCase(Locale.ROOT).trim());
-                System.out.println("Number of numOfProcesses");
-                int numOfProcesses = Integer.parseInt(scanner.nextLine());
+                System.out.println("Simulation : (1/2)");
+                String sim = scanner.nextLine();
 
-                System.out.println("Choose Parameters : (t/f)");
-                String des = scanner.nextLine();
-
-                if ('t' == des.toLowerCase(Locale.ROOT).charAt(0)) {
-
-                    System.out.println("Choose quanta time : ");
-                    int quanta = Integer.parseInt(scanner.nextLine());
-
-                    System.out.println("Choose min duration time : ");
-                    int minDur = Integer.parseInt(scanner.nextLine());
-
-                    System.out.println("Choose max time difference: ");
-                    int diff = Integer.parseInt(scanner.nextLine());
-
-                    System.out.println("Choose threshold : ");
-                    double prob = Double.parseDouble(scanner.nextLine());
-
-                    System.out.println("Choose intensity : ");
-                    int inten = Integer.parseInt(scanner.nextLine());
-
-                    System.out.println("Choose other parameters : (t/f)");
-                    des = scanner.nextLine();
-
-                    if ('t' == des.toLowerCase(Locale.ROOT).charAt(0)){
-                        System.out.println("Choose phase : ");
-                        double phase = Double.parseDouble(scanner.nextLine());
-
-                        System.out.println("Choose phase length: ");
-                        double phaselength = Double.parseDouble(scanner.nextLine());
-
-                        System.out.println("Choose multiplicant: ");
-                        int multiplicant = Integer.parseInt(scanner.nextLine());
-
-                        simulation = new Simulation2(numOfProcesses, showStatistics, quanta, minDur, inten, diff, prob,
-                                phase, phaselength, 0.1, multiplicant);
-
-                    } else {
-                        simulation = new Simulation(numOfProcesses, showStatistics, quanta, minDur, inten, diff, prob, 0.1);
-                    }
+                if ('1' == sim.toLowerCase(Locale.ROOT).charAt(0)){
+                    simulation = new Simulation(iterations, showStatistics, quantaTime, minDuration, difference, intensity);
+                } else {
+                    simulation = new Simulation2(iterations, showStatistics, quantaTime, minDuration, difference, intensity,
+                            phase, phaseLength, multiplicant);
                 }
-
-                input = input.toLowerCase(Locale.ROOT).trim();
 
                 simulation.runSimulation(input);
 
             }
-
         }
-
     }
 
- */
 }
-
